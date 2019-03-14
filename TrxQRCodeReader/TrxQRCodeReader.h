a0318cf5b777d6b2f7f71fa8e76310b6a3356b1b
Yaroslav <mail@gmail.com>
2019-03-14 14:09:06 -0400

    Initial Commit

---
+++ TrxQRCodeReader/TrxQRCodeReader.h
@@ -0,0 +1,19 @@
+//
+//  TrxQRCodeReader.h
+//  TrxQRCodeReader
+//
+//  Created by user on 14/03/2019.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrxQRCodeReader.
+FOUNDATION_EXPORT double TrxQRCodeReaderVersionNumber;
+
+//! Project version string for TrxQRCodeReader.
+FOUNDATION_EXPORT const unsigned char TrxQRCodeReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrxQRCodeReader/PublicHeader.h>
+
+
